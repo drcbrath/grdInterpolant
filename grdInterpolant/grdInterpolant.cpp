3b75990e1b55b36576f24a33de44ffd7277757ab
Curtis B. Rath <drcbrath@outlook.com>
2019-03-02 18:48:25 -0500

    On my way

--- grdInterpolant/grdInterpolant.cpp
+++ grdInterpolant/grdInterpolant.cpp
@@ -1,157 +1,165 @@
-// grdInterpolant.cpp : Defines the entry point for the console application.
-//
+// grdInterpolant.cpp
 
-#include "stdafx.h"
-#include <vector>
+// version information
+#define PROGNAME grdInterpolant
+#define MAJOR 1
+#define MINOR 0
+#define PATCH 0
+#define REV " "
+
+#define STRX(s) #s
+#define STR(s) STRX(x)
+
+const int VerMahor = MAJOR;
+const int VerMinor = MINOR;
+const int VerPatch = PATCH;
+const char VerRevision[] = REV;
+const char VerString[] = STR(PROGNAME) " v" STR(MAJOR) "." STR(MINOR) "." STR(PATCH);
+
 #include <iostream>
+#include "grdInterpolant.h"
 
 using namespace std;
 
-class bpVector: public vector<double>
+#define CHECK_BIT(var,pos) ((var) & (li64<<(pos)))
+
+int bplookup(std::vector<double> bp, double &x, int xtrpLt, int xtrpRt, size_t &nd)
 {
-public:
-	bpVector();
-	~bpVector();
-
-	int lookup(double x);
-
-private:
-
-};
-
-bpVector::bpVector()
-{
-}
-
-bpVector::~bpVector()
-{
-}
-
-int bpVector::lookup(double x)
-{
-	// bisection search for index of breakpoint element just below x,
-	//i.e. ndx such that Xs[ndx] <= x < Xs[ndx+1]
-	int na, nb, nc;
-
-	na = 0;
-	nb = this->size()-1;
-	nc = (nb - na) / 2;   // 1st guess in the middle
-
-	if(this->operator[](na) <= x && x < this->operator[](nb))
-	{
-		while (nb - na > 1)
-		{
-			if (this->operator[](nc) <= x)
-			{   // next guess in upper interval
-				na = nc;
-				nc = (nb - na) / 2;   
-			}
-			else
-			{   // next guess in lower interval
-				nb = nc;
-				nc = (nb - na) / 2;
-			}
-		}
-		return(na);
-	}
-	else if (this->operator[](nb) == x)
-	{
-		return(nb);
-	}
-
-	return(-1);
 
 }
-
-class grdInterpolant1D
-{
-public:
-
-	bpVector bp;
-	std::vector<double> table;
-
-	grdInterpolant1D();
-	grdInterpolant1D(int nx);
-	grdInterpolant1D(vector<double> xi, vector<double> yi);
-
-	~grdInterpolant1D();
-
-	double Interp(double x);
-
-private:
-
-};
 
 grdInterpolant1D::grdInterpolant1D()
 {
 }
 
-grdInterpolant1D::grdInterpolant1D(int nx)
+grdInterpolant1D::grdInterpolant1D(std::vector<double> xi, std::vector<double> yi)
 {
-	this->bp.resize(nx);
-	this->table.resize(nx);
-}
-
-grdInterpolant1D::grdInterpolant1D(vector<double> xi, vector<double> yi)
-{
-	bp.resize(xi.size());
-	for (int i = 0; i < bp.size(); i++)
-	{
-		bp[i] = xi[i];
-	}
-	table = yi;
 }
 
 grdInterpolant1D::~grdInterpolant1D()
 {
 }
 
-double grdInterpolant1D::Interp(double x)
+double grdInterpolant1D::interp(double x1)
 {
-	int ndx;
-	double y, ya, yb;
-	double xa, xb;
-	double s;
+   size_t ndx;
+   double y, ya, yb;
+   double xa, xb;
+   double s;
 
-	// find index to breakpoint just below x
-	ndx = bp.lookup(x);
+   // find index to breakpoint just below x
+   if (bplookup(bp1, x1, xtrpLt, xtrpRt, ndx))
+      return(std::numeric_limits<double>::quiet_NaN());
 
-	// get breakpoints below and above x
-	xa = bp[ndx];
-	xb = bp[ndx + 1];
-	ya = table[ndx];
-	yb = table[ndx + 1];
+   // get breakpoints below and above x
+   xa = bp1[ndx];
+   xb = bp1[ndx + 1];
+   ya = table[ndx];
+   yb = table[ndx + 1];
 
-	// linearly interpolate from table
-	s = (x - xa) / (xb - xa);
-	y = (1 - s)*ya + s*yb;
+   // linearly interpolate from table
+   s = (x1 - xa) / (xb - xa);
+   y = (1 - s)*ya + s * yb;
 
-	return(y);
+   return(y);
 }
 
-int main()
+
+grdInterpolant2D::grdInterpolant2D()
 {
-	int nx = 5;
-	double xi[] = { 0.0, 1.1, 2.2, 3.3, 4.4 };
-	double yi[] = { 0.0, 1.21, 4.84, 10.89, 17.76 };
-
-	vector<double> xv = { 0.0, 1.1, 2.2, 3.3, 4.4 };
-	vector<double> yv = { 0.0, 1.21, 4.84, 10.89, 17.76 };
-
-	double x;
-
-	grdInterpolant1D gi(5);
-
-	for (int i = 0; i <= nx - 1; i++)
-	{
-		gi.bp[i] = xi[i];
-		gi.table[i] = yi[i];
-	}
-
-	x = gi.Interp(0.55);
-	cout << x << endl;
-
-	grdInterpolant1D gv(xv, yv);
-
-	return 0;
 }
+
+grdInterpolant2D::grdInterpolant2D(std::vector<double> bp1, std::vector<double> bp2, std::vector< std::vector<double> > table)
+{
+}
+
+grdInterpolant2D::~grdInterpolant2D()
+{
+}
+
+double grdInterpolant2D::interp(double x1, double x2)
+{
+   return 0.0;
+}
+
+
+
+grdInterpolant3D::grdInterpolant3D()
+{
+}
+
+grdInterpolant3D::grdInterpolant3D(std::vector<double>bp1, std::vector<double>bp2, std::vector<double>bp3, std::vector< std::vector< std::vector<double> > >table)
+{
+}
+
+grdInterpolant3D::~grdInterpolant3D()
+{
+}
+
+double grdInterpolant3D::interp(double x1, double x2, double x3)
+{
+   return 0.0;
+}
+
+
+
+grdInterpolantND::grdInterpolantND()
+{
+}
+
+grdInterpolantND::grdInterpolantND(std::vector<std::vector<double>> bps, std::vector<double> table)
+{
+}
+
+grdInterpolantND::~grdInterpolantND()
+{
+}
+
+double grdInterpolantND::interp(std::vector<double> x)
+{
+   return 0.0;
+}
+
+double grdInterpolantND::interps(std::vector<double> x)
+{
+   return 0.0;
+}
+
+//------------------------------------------------------------------------------
+
+
+//int lookup(double x)
+//{
+//	// bisection search for index of breakpoint element just below x,
+//	//i.e. ndx such that Xs[ndx] <= x < Xs[ndx+1]
+//	int na, nb, nc;
+//
+//	na = 0;
+//	nb = this->size()-1;
+//	nc = (nb - na) / 2;   // 1st guess in the middle
+//
+//	if(this->operator[](na) <= x && x < this->operator[](nb))
+//	{
+//		while (nb - na > 1)
+//		{
+//			if (this->operator[](nc) <= x)
+//			{   // next guess in upper interval
+//				na = nc;
+//				nc = (nb - na) / 2;   
+//			}
+//			else
+//			{   // next guess in lower interval
+//				nb = nc;
+//				nc = (nb - na) / 2;
+//			}
+//		}
+//		return(na);
+//	}
+//	else if (this->operator[](nb) == x)
+//	{
+//		return(nb);
+//	}
+//
+//	return(-1);
+//
+//}
--- grdInterpolant/grdInterpolant.h
+++ grdInterpolant/grdInterpolant.h
@@ -1,2 +1,176 @@
 #pragma once
 
+#include <array>
+#include <vector>
+
+//------------------------------------------------------------------------------
+
+enum xtraps { NONE, NEAREST, LINEAR };
+
+class grdInterpolant1D
+{
+public:
+   // extrapolation flags; default linear on left and right end of breakpoint vectors
+   int xtrpLt = LINEAR;
+   int xtrpRt = LINEAR;
+
+   std::vector<double> bp1;      // vector of breakpoints for 1st independent variable
+   std::vector<double> table;    // 1-D table as a vector, values to interpolate
+
+   grdInterpolant1D();
+   grdInterpolant1D(std::vector<double> xi, std::vector<double> yi);
+
+   ~grdInterpolant1D();
+
+   double interp(double x);
+
+private:
+
+};
+
+grdInterpolant1D::grdInterpolant1D()
+{
+}
+
+grdInterpolant1D::grdInterpolant1D(std::vector<double> xi, std::vector<double> yi)
+{
+}
+
+grdInterpolant1D::~grdInterpolant1D()
+{
+}
+
+//double grdInterpolant1D::interp(double x)
+//{
+//}
+
+class grdInterpolant2D
+{
+public:
+   // extrapolation flags; default linear on left and right end of breakpoint vectors
+   std::vector<int> xtrpLt;
+   std::vector<int> xtrpRt;
+
+   std::vector<double> bp1;      // vector of breakpoints for independent variable 1
+   std::vector<double> bp2;      // vector of breakpoints for independent variable 2
+   std::vector<double> table;    // 2-D table as a vector, values to interpolate
+
+   grdInterpolant2D();
+   grdInterpolant2D(std::vector<double> bp1, std::vector<double> bp2, std::vector< std::vector<double> > table);
+
+   ~grdInterpolant2D();
+
+   double interp(double x1, double x2);
+
+private:
+
+};
+
+grdInterpolant2D::grdInterpolant2D()
+{
+}
+
+grdInterpolant2D::grdInterpolant2D(std::vector<double> bp1, std::vector<double> bp2, std::vector< std::vector<double> > table)
+{
+}
+
+grdInterpolant2D::~grdInterpolant2D()
+{
+}
+
+double grdInterpolant2D::interp(double x1, double x2)
+{
+   return 0.0;
+}
+
+class grdInterpolant3D
+{
+public:
+   // extrapolation flags; default linear on left and right end of breakpoint vectors
+   std::vector<int> xtrpLt;
+   std::vector<int> xtrpRt;
+
+   std::vector<double> bp1;      // vector of breakpoints for independent variable 1
+   std::vector<double> bp2;      // vector of breakpoints for independent variable 2
+   std::vector<double> bp3;      // vector of breakpoints for independent variable 4
+   std::vector<double> table;    // 3-D table as a vector, values to interpolate
+
+   grdInterpolant3D();
+   grdInterpolant3D(std::vector<double>bp1, std::vector<double>bp2, std::vector<double>bp3, std::vector< std::vector< std::vector<double> > >table);
+
+   ~grdInterpolant3D();
+
+   double interp(double x1, double x2, double x3);
+
+private:
+
+};
+
+grdInterpolant3D::grdInterpolant3D()
+{
+}
+
+grdInterpolant3D::grdInterpolant3D(std::vector<double>bp1, std::vector<double>bp2, std::vector<double>bp3, std::vector< std::vector< std::vector<double> > >table)
+{
+}
+
+grdInterpolant3D::~grdInterpolant3D()
+{
+}
+
+double grdInterpolant3D::interp(double x1, double x2, double x3)
+{
+   return 0.0;
+}
+
+class grdInterpolantND
+{
+public:
+   size_t nDims;   // number of dimensions, aka tensor rank
+   size_t nverts;   // number of table vertices surrounding inputpoint (x1, x2, x3, ..., xn) at which to interpolate
+   std::vector<size_t> sizes; // vector of size of each dimension (like return from matlab size(table) )
+   std::vector<int> xtrpLt;
+   std::vector<int> xtrpRt;
+
+   std::vector< std::array<char, 2> > exfs;   // vector of extrapolation flags ?
+   std::vector< std::vector<double> > bps;   // vector of breakpoint vectors for independent variables
+
+   std::vector<double> table;   // N-D table as a vector (needs indexing function to access like a multi-dimensional table)
+
+   grdInterpolantND();
+   grdInterpolantND(std::vector< std::vector<double> > bps, std::vector<double> table);
+
+   ~grdInterpolantND();
+
+   double interp(std::vector<double> x);    // handles singleton dimensions
+   double interps(std::vector<double> x);   // for speed, but does not handle singleton dimensions
+
+private:
+   std::vector<size_t> Nofs;   // vector of offsets from subscript indices into table(j1,j2,j3, ..., jn) to linear index table(i)
+   std::vector<size_t> nds;    // vector of indices found from lookup of input x into each breakpoint vector
+   std::vector<double> xf;     // vector of graction of distance of xi between dim[m] endpoints; i.e. xa --- xi ------xb, xf = dist(xa,xi)/dist(xa,xb)
+   std::vector<double> cxf;    // vector of 1-xf
+
+};
+
+grdInterpolantND::grdInterpolantND()
+{
+}
+
+grdInterpolantND::grdInterpolantND(std::vector<std::vector<double>> bps, std::vector<double> table)
+{
+}
+
+grdInterpolantND::~grdInterpolantND()
+{
+}
+
+double grdInterpolantND::interp(std::vector<double> x)
+{
+   return 0.0;
+}
+
+double grdInterpolantND::interps(std::vector<double> x)
+{
+   return 0.0;
+}
